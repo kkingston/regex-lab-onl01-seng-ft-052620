61af101cb38a44289beb656a0eeb93f280e83829
Kayla Kingston <kkingston09@gmail.com>
2020-06-11 11:05:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,13 +17,13 @@ def words_five_letters_long(text)
 
 end
 
-# def first_word_capitalized_and_ends_with_punctuation?(text)
-#   if text.match(\^[A-Z].*[a-zA-Z][!?.]$\)
-#     true 
-#   else
-#     false 
-#   end 
-# end
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(\^[A-Z].*[a-zA-Z][!?.]$\)
+    true 
+  else
+    false 
+  end 
+end
 
 def valid_phone_number?(phone)
   if phone.match(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/)
